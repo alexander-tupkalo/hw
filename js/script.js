e8432315d1278a4ea56b8decf74a93a171d2b4cd
unknown <alexandertupkalo2@gmail.com>
2026-08-18 18:40:32 +0300

    hw-16

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-'use stickt'
+"use strict";
 
-console.log(123)
+const generateKey = (length, characters) => {
+    let result = "";
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+
+        const randomChar = characters[randomIndex];
+
+        result += randomChar;
+    }
+    
+    return result
+};
+
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+const key = generateKey(16, characters);
+
+console.log(key);
